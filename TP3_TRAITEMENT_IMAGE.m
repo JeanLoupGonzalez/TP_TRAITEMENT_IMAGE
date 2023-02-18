ef4445f1a2dcdd8cd4346ebd646ef1b4f7affcb5
JeanLoupGonzalez <116089205+JeanLoupGonzalez@users.noreply.github.com>
2023-02-18 10:37:49 +0100

    Update TP3_TRAITEMENT_IMAGE.m

--- TP3_TRAITEMENT_IMAGE.m
+++ TP3_TRAITEMENT_IMAGE.m
@@ -1,22 +1,18 @@
 %//////////////PARTIE 1 FILTRE MEDIAN ET DECONVOLUTION/////////
-img=double(imread('.\image_poteau.png'));
+img=double(imread("/MATLAB Drive/image_poteau.png"));
 %on ne garde qu'une composante parmi les 3 couleurs
 %pour avoir une matrice de dimension 2
 img=img(:,:,1);
-
-%affiche l'image de base en niveaux de gris, non bruitÈe
-image(uint8(img));
-colormap(gray(256));
 
 %on bruite l'image manuellement
 img_bruitee=img;
 S=size(img_bruitee);
 
-debut=550;
-pas=1;
-max=560;
-listeEQMbruit=[];
-listeEQMmedian=[];
+% debut=550;
+% pas=1;
+% max=560;
+% listeEQMbruit=[];
+% listeEQMmedian=[];
 % for n = debut:pas:max
 %     for i = 1:n
 %         %tire un pixel au hasard
@@ -30,15 +26,13 @@ listeEQMmedian=[];
 %         %le met a 255
 %         img_bruitee(x,y)=255;
 %     end
-%     %affiche l'image bruitÈe
-%     image(uint8(img_bruitee));
 % 
 %     %calcul de l'EQM entre image originale et image bruitee
 %     EQM=sum(sum((img-img_bruitee).^2))/(S(1)*S(2));
 %     listeEQMbruit = [listeEQMbruit,EQM]
 %     %fprintf('EQM entre image originale et image bruitee  = %d\n',EQM)
 % 
-%     % on applique un filtre mÈdian sur l'image bruitÈe
+%     % on applique un filtre m√©dian sur l'image bruit√©e
 %     % 
 %     img_bruitee_filtre_median=img_bruitee;
 %     for i = 1:S(1)-4
@@ -52,53 +46,75 @@ listeEQMmedian=[];
 %             img_bruitee_filtre_median(i+2,j+2)= vect_intensites_trie(13);
 %         end
 %     end
-%     % affichage de cette image bruitee filtree par filtre median
-%     % image(uint8(img_bruitee_filtre_median));
-%     % on remarque que sur les bords de l'image (sur une bande 
-%     % de 4 pixels, l'image n'est pas filtree
 % 
 %     %calcul EQM entre image bruitee filtree par filtre median et img originale
 %     EQM_median=sum(sum((img-img_bruitee_filtre_median).^2))/(S(1)*S(2));
 %     listeEQMmedian = [listeEQMmedian,EQM_median]
 %     %fprintf('EQM entre image originale et image bruitee filtree  = %d\n',EQM_median)
 % end
+% 
+% %affiche l'image de base en niveaux de gris, non bruit√©e
+% figure
+% image(uint8(img));
+% colormap(gray(256));
+% 
+% %affiche l'image bruit√©e
+% figure
+% image(uint8(img_bruitee));
+% colormap(gray(256));
+% 
+% %affichage de cette image bruitee filtree par filtre median
+% figure
+% image(uint8(img_bruitee_filtre_median));
+% colormap(gray(256));
+% % on remarque que sur les bords de l'image (sur une bande 
+% % de 4 pixels, l'image n'est pas filtree
+% 
+% %affiche les courbes EQM
+% figure
 % plot(listeEQMbruit)
 % hold on
 % plot(listeEQMmedian)
 % hold off
 
 %////////////////CONCLUSION/////////////////////////////////
-%‡ partir de 556 pixels de bruit (environ),
-%l'EQM du filtrage mÈdian sur l'image bruitee est 
-%inferieure ‡ l'EQM de l'image bruitee
-%---> il est donc intÈressant de rÈaliser un filtrage mÈdian
-%sur une image bruitÈe ‡ partir de 556 pixels de bruit environ
-
-%//////////////PARTIE 2 : MASQUE FLOU////////////////////////
-img2=double(imread('.\img_flou.png'));
+%√† partir de 556 pixels de bruit (environ),
+%l'EQM du filtrage m√©dian sur l'image bruitee est 
+%inferieure √† l'EQM de l'image bruitee
+%---> il est donc int√©ressant de r√©aliser un filtrage m√©dian
+%sur une image bruit√©e √† partir de 556 pixels de bruit environ
+% 
+% %//////////////PARTIE 2 : MASQUE FLOU////////////////////////
+img2=double(imread("/MATLAB Drive/img_floue.png"));
 %pour avoir matrice de dimension 2
 img2=img2(:,:,1);
-%un prend un filtre "assez" grand pr avoir un flou "prononcÈ"
+%un prend un filtre "assez" grand pr avoir un flou "prononc√©"
 %attention, on divise par taille_filtre^2
 filtre_moyenne_passe_bas=ones(9)/81
-%on rÈalise la convolution en gardant la taille de l'image convoluee
-%intacte puis plot les 2 images
-figure(1)
-img2_floutee=conv2(img2,filtre_moyenne_passe_bas,'same')
-image(uint8(img2_floutee))
-figure(2)
+
+%img de base
+figure
 image(uint8(img2))
 colormap(gray(256));
+
+%on r√©alise la convolution en gardant la taille de l'image convoluee
+%intacte puis plot les 2 images
+figure
+img2_floutee=conv2(img2,filtre_moyenne_passe_bas,'same')
+image(uint8(img2_floutee))
+colormap(gray(256));
+
 %masque flou
 img_masque_flou=img2-img2_floutee;
-figure(3)
+figure
 image(uint8(img_masque_flou))
 colormap(gray(256));
+
 %amplification de la partie nette
 coeff=2;
 img_amplifie_nette=img_masque_flou*coeff;
 %report sur image de depart
 img_apres_masque_flou=img2-img_amplifie_nette;
-figure(4)
+figure
 image(uint8(img_apres_masque_flou))
 colormap(gray(256));
